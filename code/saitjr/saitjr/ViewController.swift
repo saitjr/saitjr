763e865b8a81a5228728d724882d788dd114e601
TangJR <tangjr.work@gmail.com>
2015-12-30 10:26:22 +0800

    add code file

---
+++ code/saitjr/saitjr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  saitjr
+//
+//  Created by TangJR on 12/30/15.
+//  Copyright © 2015 tangjr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
